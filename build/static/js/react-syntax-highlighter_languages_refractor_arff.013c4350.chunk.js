f96af9a539950aa2ccad1e7d20ceb7c805752e9f
thezaeemaanwar <thezaeemaanwar@gmail.com>
2022-06-15 13:55:00 +0500

    NEW: Build

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_arff.013c4350.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkakool_documentaion=self.webpackChunkakool_documentaion||[]).push([[1438],{7209:function(a){function e(a){a.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}a.exports=e,e.displayName="arff",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.013c4350.chunk.js.map
